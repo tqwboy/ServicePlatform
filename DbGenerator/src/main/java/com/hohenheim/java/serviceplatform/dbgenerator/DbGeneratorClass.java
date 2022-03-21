3ff385f06dbca4b0e294e2b2154b024c0b36b17c
Hohenheim <tqwboy@163.com>
2022-03-22 00:22:51 +0800

    1. 升级部分第三方库；
    2. 添加独立 module，名为 DbGenerator，用于映射数据表生成对应的 Entity、Mapper、DAO；
    3. 完善配置，并将配置中的敏感数据进行加密、解密处理。

---
+++ DbGenerator/src/main/java/com/hohenheim/java/serviceplatform/dbgenerator/DbGeneratorClass.java
@@ -0,0 +1,12 @@
+package com.hohenheim.java.serviceplatform.dbgenerator;
+
+/**
+ * @author Hohenheim
+ * @date 2022/3/20
+ * @description
+ */
+public class DbGeneratorClass {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/hohenheim/java/serviceplatform/ServicePlatformApplication.java
+++ src/main/java/com/hohenheim/java/serviceplatform/ServicePlatformApplication.java
@@ -2,8 +2,10 @@ package com.hohenheim.java.serviceplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.hohenheim.java.serviceplatform")
+@Import(cn.hutool.extra.spring.SpringUtil.class)
 public class ServicePlatformApplication {
 
     public static void main(String[] args) {
